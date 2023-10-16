a808a6f9c83c608493639549677e7fcaf6724dec
Bernard Paes <89420728+BernardPaes@users.noreply.github.com>
2023-10-16 20:27:09 +0000

    Atualiza página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Pontificia Universidade Católica de Minas Gerais - Ciências da Computação</h1>
+    
+    return <h1>Pontificia Universidade Católica de Minas Gerais - Ciências da Computação - Belo Horizonte</h1>
 }
 
 export default Home;    
